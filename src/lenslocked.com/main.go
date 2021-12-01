a2720807daf7febc44f182c96de8e57fafa8e8fb
eitah <eli@spantree.net>
2021-11-30 21:04:34 -0600

    Renders footer template in a DRY way

---
+++ src/lenslocked.com/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/eitah/lenslocked/src/lenslocked.com/views"
+	"github.com/gorilla/mux"
+)
+
+var homeView *views.View
+var contactView *views.View
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	if err := homeView.Template.Execute(w, nil); err != nil {
+		panic(err)
+	}
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	if err := contactView.Template.Execute(w, nil); err != nil {
+		panic(err)
+	}
+}
+
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintln(w, "<h1>Frequently Asked Questions</h1><p>This is the frequently asked questions page</p>")
+}
+
+func fourOhFour(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintln(w, "<h2>Page not found</h2>")
+}
+
+func main() {
+	homeView = views.NewView("views/home.gohtml")
+	contactView = views.NewView("views/contact.gohtml")
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
+	r.NotFoundHandler = http.HandlerFunc(fourOhFour)
+	fmt.Println("Server starting on :3000...")
+	http.ListenAndServe(":3000", r)
+}
