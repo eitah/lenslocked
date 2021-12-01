c4504259221e755dfe53bccfd53a2984d2c4c22c
eitah <eli@spantree.net>
2021-11-30 22:20:26 -0600

    Adds a pay me money page with an alternate layout. Its pretty slick

--- src/lenslocked.com/main.go
+++ src/lenslocked.com/main.go
@@ -12,6 +12,7 @@ var homeView *views.View
 var contactView *views.View
 var faqView *views.View
 var fourOhFourView *views.View
+var payMeMoneyView *views.View
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
@@ -27,6 +28,11 @@ func faq(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	must(faqView.Render(w, nil))
 
+}
+
+func payMeMoney(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	must(payMeMoneyView.Render(w, nil))
 }
 
 func fourOhFour(w http.ResponseWriter, r *http.Request) {
@@ -46,11 +52,14 @@ func main() {
 	contactView = views.NewView("bootstrap", "views/contact.gohtml")
 	faqView = views.NewView("bootstrap", "views/faq.gohtml")
 	fourOhFourView = views.NewView("bootstrap", "views/fourohfour.gohtml")
+	payMeMoneyView = views.NewView("bootstrap-nonav", "views/pay-me-money.gohtml")
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 	r.HandleFunc("/faq", faq)
+	r.HandleFunc("/pay-me-money", payMeMoney)
+
 	r.NotFoundHandler = http.HandlerFunc(fourOhFour)
 	fmt.Println("Server starting on :3000...")
 	http.ListenAndServe(":3000", r)
