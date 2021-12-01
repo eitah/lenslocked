614e2787345ccf89d4bd620edd75bd2cec6d1f39
eitah <eli@spantree.net>
2021-11-30 20:03:29 -0600

    Adds chapter 3 content with gorilla router

--- main.go
+++ main.go
@@ -3,41 +3,36 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Welcome to my great site!</h1>")
 }
 
-func contactHandler(w http.ResponseWriter, r *http.Request) {
+func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Contact page</h1><p>To get in touch with me, reach out to <a href=\"mailto:elijahbit@gmail.com\">elijahbit@gmail.com</a>.</p>")
 }
 
-func fourohfourHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNotFound)
+func faq(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprintln(w, "<h1>Zomg page not found!</h1>")
-
+	fmt.Fprintln(w, "<h1>Frequently Asked Questions</h1><p>This is the frequently asked questions page</p>")
 }
 
-type Router struct{}
-
-func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	default:
-		fourohfourHandler(w, r)
-	}
+func fourOhFour(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintln(w, "<h2>Page not found</h2>")
 }
 
 func main() {
-	var router Router
-	fmt.Println("Starting the server on :3000")
-	http.ListenAndServe(":3000", router)
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
+	r.NotFoundHandler = http.HandlerFunc(fourOhFour)
+	http.ListenAndServe(":3000", r)
 }
