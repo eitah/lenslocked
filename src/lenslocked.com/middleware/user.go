4988695b9ae6493de4f7e1a4ecabc36d8f40c2a7
eitah <eli@spantree.net>
2021-12-31 22:33:34 -0600

    Bypass user token lookup if it's a static asset

--- src/lenslocked.com/middleware/user.go
+++ src/lenslocked.com/middleware/user.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/eitah/lenslocked/src/lenslocked.com/context"
 	"github.com/eitah/lenslocked/src/lenslocked.com/models"
@@ -17,6 +18,13 @@ func (mw *User) Apply(next http.Handler) http.HandlerFunc {
 
 func (mw *User) ApplyFn(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		if strings.HasPrefix(path, "/assets/") ||
+			strings.HasPrefix(path, "/images/") {
+			// static asset and images dont need a user token from db so bypass
+			next(w, r)
+			return // the final return prevents execution after the next call.
+		}
 		cookie, err := r.Cookie("remember_token")
 		if err != nil {
 			next(w, r)
