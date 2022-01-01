cf8ccfaebdc0b57830f6b473dcee72557c9b1b5f
eitah <eli@spantree.net>
2021-12-31 21:45:13 -0600

    Assets served via a stylesheet

--- src/lenslocked.com/main.go
+++ src/lenslocked.com/main.go
@@ -87,6 +87,11 @@ func main() {
 	imageHandler := http.FileServer(http.Dir("./images/"))
 	r.PathPrefix("/images/").Handler(http.StripPrefix("/images/", imageHandler))
 
+	// Assets
+	assetHandler := http.FileServer(http.Dir("./assets/"))
+	assetHandler = http.StripPrefix("/assets/", assetHandler)
+	r.PathPrefix("/assets/").Handler(assetHandler)
+
 	r.NotFoundHandler = http.HandlerFunc(fourOhFour)
 	fmt.Println("Starting server on http://localhost:3000")
 	http.ListenAndServe(":3000", userMW.Apply(r))
